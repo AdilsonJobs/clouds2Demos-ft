b265aa2a171149abf535f0db4798e943cae273e1
Adilson Jobs <97887151+AdilsonJobs@users.noreply.github.com>
2024-06-06 11:22:06 +0100

    Update variables.tf

--- Instance-Clouds2/variables.tf
+++ Instance-Clouds2/variables.tf
@@ -1,6 +1,6 @@
 variable "accesskey" {
-  default = "e46df526feba4c1884cbe97916f0cddc" # 
+  default = "e46df526feba4c1884cbe97916f0cddc" # alterar a acess key
 }
 variable "secretkey" {
-  default = "ec6f759b1a3d42d7b64add42b8726a1e"
+  default = "ec6f759b1a3d42d7b64add42b8726a1e" # alterar a private key
 }
